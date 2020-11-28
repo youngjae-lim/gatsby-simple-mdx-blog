4386103198032f608aa9d989589a98ff8a5f1c66
Youngjae <limyoungjae@gmail.com>
2020-11-27 23:28:02 -0500

    Added google fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,5 +44,15 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `roboto mono`,
+          `muli\:400,400i,700,700i`, // you can also specify font weights and styles
+        ],
+        display: "swap",
+      },
+    },
   ],
 }
