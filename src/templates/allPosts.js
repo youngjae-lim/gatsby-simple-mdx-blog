62d55e99583589d5b510178e2455d8727cbb3e02
Youngjae <limyoungjae@gmail.com>
2020-11-30 00:20:53 -0500

    Added paginated pages programmatically

---
+++ src/templates/allPosts.js
@@ -0,0 +1,74 @@
+import React from "react"
+import { graphql } from "gatsby"
+import {
+  Container,
+  Content,
+  ContentCard,
+  FeatureImage,
+  Pagination,
+} from "../components"
+import { H1, P } from "../elements"
+
+const allPosts = ({ pageContext, data }) => {
+  const { currentPage, numPages } = pageContext
+  const isFirst = currentPage === 1
+  const isLast = currentPage === numPages
+  const prevPage = currentPage - 1 === 1 ? "/" : `/${currentPage - 1}`
+  const nextPage = `/${currentPage + 1}`
+
+  const posts = data.allMdx.edges
+
+  return (
+    <Container>
+      <FeatureImage />
+      <Content>
+        <H1 textAlign="center" margin="0 0 1rem 0">
+          Lorem ipsum dolor sit amet.
+        </H1>
+        <P color="dark2" textAlign="center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem quidem
+          voluptatum nihil rerum ad mollitia doloremque sint provident odio
+          necessitatibus.
+        </P>
+        {posts.map(post => (
+          <ContentCard
+            key={post.node.frontmatter.slug}
+            date={post.node.frontmatter.date}
+            title={post.node.frontmatter.title}
+            excerpt={post.node.frontmatter.excerpt}
+            slug={post.node.frontmatter.slug}
+          />
+        ))}
+      </Content>
+      <Pagination
+        isFirst={isFirst}
+        isLast={isLast}
+        prevPage={prevPage}
+        nextPage={nextPage}
+      />
+    </Container>
+  )
+}
+
+export const pageQuery = graphql`
+  query AllPostsQuery($skip: Int!, $limit: Int!) {
+    allMdx(
+      sort: { fields: frontmatter___date, order: DESC }
+      skip: $skip
+      limit: $limit
+    ) {
+      edges {
+        node {
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            excerpt
+            slug
+          }
+        }
+      }
+    }
+  }
+`
+
+export default allPosts
